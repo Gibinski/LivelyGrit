2302a7875b5936c110968decf2eb0bb3c2837bd6
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-07-26 18:44:04 +0200

    authentication

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 # .\main\views.py
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
