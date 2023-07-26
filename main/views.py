9548663a135c724a65b2fcd6bf0b72efa49b224f
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-07-26 19:05:02 +0200

    Create an Admin page with statistics

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import VisitorData
 
 def index(request):
     return render(request, 'index.html')
@@ -9,3 +10,8 @@ def index(request):
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def statistics(request):
+    visitor_data = VisitorData.objects.all()
+    return render(request, 'statistics.html', {'visitor_data': visitor_data})
