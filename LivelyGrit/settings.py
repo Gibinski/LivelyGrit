bb034f8c4f2f2e86fcd1e553788a946fbde61f3e
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-07-26 17:56:13 +0200

    Add

--- LivelyGrit/settings.py
+++ LivelyGrit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_s+lb7tntgc-=28w138)*hzn*&&jc*1186enl(k_ksj8!l3l@)'
+SECRET_KEY = 'django-insecure-(=ppc-o*&ar4*4qi&3)#^ivghzbhi+zczjiv*!t=&e5b6i9qio'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
