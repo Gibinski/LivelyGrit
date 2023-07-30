4b55094e4ba2d42052304497564d283d3a30b97d
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-07-30 17:38:30 +0200

    Add home page

--- livelygrit_project/livelygrit_app/views.py
+++ livelygrit_project/livelygrit_app/views.py
@@ -1,3 +1,24 @@
+import random
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    # specify a path to the files with the motivating sentences
+    bg_file_path = 'motivational_bg.txt'
+    en_file_path = 'motivational_en.txt'
+
+    # open the files and read the sentences
+    with open(bg_file_path, encoding='utf-8') as bg_file:
+        bg_lines = bg_file.readlines()
+
+    with open(en_file_path, encoding='utf-8') as en_file:
+        en_lines = en_file.readlines()
+
+    # choose a random sentence for the current language
+    current_language = request.LANGUAGE_CODE
+    if current_language == 'bg':
+        motivational_line = random.choice(bg_lines).strip()
+    else:
+        motivational_line = random.choice(en_lines).strip()
+
+    # provide the sentence as the template context
+    return render(request, 'livelygrit_app/home.html', {'motivational_line': motivational_line})
