805c7c179db71826667e173d48f94d07df7a618c
Lyubomir Gibinski <l.gibinski@gmail.com>
2023-07-31 00:38:14 +0200

    Django files changes

--- LivelyGrit/main/views.py
+++ LivelyGrit/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Define view functions for each page
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
